e0b3ec5f4127ec11702776c8ad44a5647f4b9c89
LuanRoger <luan.roger.2003@gmail.com>
2023-09-25 09:52:38 -0300

    feat: print all results

--- BestPath/Program.cs
+++ BestPath/Program.cs
@@ -1,84 +1,59 @@
-﻿using BestPath.Algos.Bfs;
-using BestPath.Algos.Dfs;
+﻿using BestPath;
+using BestPath.Algos.AStar;
+using BestPath.Algos.Bfs;
+using BestPath.Algos.Ucs;
 using BestPath.Graph.Base;
 
-Console.WriteLine("BFS:");
-RunBfs();
-Console.WriteLine("========================================");
-Console.WriteLine("DFS:");
-RunDfs();
+Console.WriteLine("Lendo grafo...");
+var aStarParseTask = ReadAStarGraph();
+var bfsParseTask = ReadBfsGraph();
+var ucsParseTask = ReadUcsGraph();
+await Task.WhenAll(aStarParseTask, ucsParseTask);
+Console.WriteLine("Grafo lido!");
 
-void RunBfs()
+AStarGraph aStarGraph = aStarParseTask.Result; 
+BfsGraph bfsGraph = bfsParseTask.Result;
+UcsGraph ucsGraph = ucsParseTask.Result;
+GC.Collect();
+
+NodeRef nodeFromAStar = aStarGraph.GetSomeNodeRef(89);
+NodeRef nodeToAStar = aStarGraph.GetSomeNodeRef(27);
+NodeRef nodeFromBfs = bfsGraph.GetSomeNodeRef(89);
+NodeRef nodeToBfs = bfsGraph.GetSomeNodeRef(27);
+NodeRef nodeFromUcs = ucsGraph.GetSomeNodeRef(89);
+NodeRef nodeToUcs = ucsGraph.GetSomeNodeRef(27);
+
+
+Console.WriteLine("Executando algoritmos...");
+var bfsAlgo = Task.Run(() => bfsGraph.RunAlgo(nodeFromBfs, nodeToBfs));
+var ucsAlgo = Task.Run(() => ucsGraph.RunAlgo(nodeFromUcs, nodeToUcs));
+var aStarAlgo = Task.Run(() => aStarGraph.RunAlgo(nodeFromAStar, nodeToAStar));
+await Task.WhenAll(aStarAlgo, ucsAlgo);
+Console.WriteLine("Algoritmos executados!");
+
+Console.WriteLine("Resultado do A*: ");
+Console.WriteLine(aStarAlgo.Result);
+Console.WriteLine("Resultado do BFS: ");
+Console.WriteLine(bfsAlgo.Result);
+Console.WriteLine("Resultado UCS: ");
+Console.WriteLine(ucsAlgo.Result);
+
+return;
+
+async Task<BfsGraph> ReadBfsGraph()
 {
-    BfsGraph graph = new();
-
-    BfsNode node0 = new(0);
-    BfsNode node1 = new(1);
-    BfsNode node2 = new(2);
-    BfsNode node3 = new(3);
-    BfsNode node4 = new(4);
-    BfsNode node5 = new(5);
-    BfsNode node6 = new(6);
-    BfsNode node7 = new(7);
-
-    NodeRef node0Ref = graph.AddNode(node0);
-    NodeRef node1Ref = graph.AddNode(node1);
-    NodeRef node2Ref = graph.AddNode(node2);
-    NodeRef node3Ref = graph.AddNode(node3);
-    NodeRef node4Ref = graph.AddNode(node4);
-    NodeRef node5Ref = graph.AddNode(node5);
-    NodeRef node6Ref = graph.AddNode(node6);
-    NodeRef node7Ref = graph.AddNode(node7);
-
-    graph.AddEdge(new(node0Ref, node1Ref));
-    graph.AddEdge(new(node1Ref, node3Ref));
-    graph.AddEdge(new(node2Ref, node1Ref));
-    graph.AddEdge(new(node3Ref, node2Ref));
-    graph.AddEdge(new(node3Ref, node4Ref));
-    graph.AddEdge(new(node4Ref, node5Ref));
-    graph.AddEdge(new(node5Ref, node7Ref));
-    graph.AddEdge(new(node7Ref, node6Ref));
-    graph.AddEdge(new(node6Ref, node4Ref));
-
-    BfsResultSnapshot result = graph.RunAlgo(node1Ref, node6Ref);
-
-
-    Console.WriteLine(result);
+    GraphParser graphParser = new(@"C:\\Users\\luanr\\Documents\\Jobs\\c_sharp\\BestPath\\BestPath\\inputs\\ny\\USA-ny-coordinates", @"C:\\Users\\luanr\\Documents\\Jobs\\c_sharp\\BestPath\\BestPath\\inputs\\ny\\USA-ny-distance");
+    return await graphParser.ParseToBfsGraph();
 }
 
-void RunDfs()
+async Task<AStarGraph> ReadAStarGraph()
 {
-    DfsGraph graph = new();
+    GraphParser graphParser = new(@"C:\\Users\\luanr\\Documents\\Jobs\\c_sharp\\BestPath\\BestPath\\inputs\\ny\\USA-ny-coordinates", @"C:\\Users\\luanr\\Documents\\Jobs\\c_sharp\\BestPath\\BestPath\\inputs\\ny\\USA-ny-distance");
+    return await graphParser.ParseToAStarGraph();
+}
 
-    DfsNode node0 = new(0);
-    DfsNode node1 = new(1);
-    DfsNode node2 = new(2);
-    DfsNode node3 = new(3);
-    DfsNode node4 = new(4);
-    DfsNode node5 = new(5);
-    DfsNode node6 = new(6);
-    DfsNode node7 = new(7);
-
-    NodeRef node0Ref = graph.AddNode(node0);
-    NodeRef node1Ref = graph.AddNode(node1);
-    NodeRef node2Ref = graph.AddNode(node2);
-    NodeRef node3Ref = graph.AddNode(node3);
-    NodeRef node4Ref = graph.AddNode(node4);
-    NodeRef node5Ref = graph.AddNode(node5);
-    NodeRef node6Ref = graph.AddNode(node6);
-    NodeRef node7Ref = graph.AddNode(node7);
-
-    graph.AddEdge(new(node0Ref, node1Ref));
-    graph.AddEdge(new(node1Ref, node3Ref));
-    graph.AddEdge(new(node2Ref, node1Ref));
-    graph.AddEdge(new(node3Ref, node2Ref));
-    graph.AddEdge(new(node3Ref, node4Ref));
-    graph.AddEdge(new(node4Ref, node5Ref));
-    graph.AddEdge(new(node5Ref, node7Ref));
-    graph.AddEdge(new(node7Ref, node6Ref));
-    graph.AddEdge(new(node6Ref, node4Ref));
-
-    DfsResultSnapshot result = graph.RunAlgo(node1Ref, node6Ref);
-
-    Console.WriteLine(result);
+async Task<UcsGraph> ReadUcsGraph()
+{
+    GraphParser graphParser = new(@"C:\\Users\\luanr\\Documents\\Jobs\\c_sharp\\BestPath\\BestPath\\inputs\\ny\\USA-ny-coordinates", @"C:\\Users\\luanr\\Documents\\Jobs\\c_sharp\\BestPath\\BestPath\\inputs\\ny\\USA-ny-distance");
+    return await graphParser.ParseToUcsGraph();
 }
